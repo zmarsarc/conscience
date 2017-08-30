58f0d1ad87899e07485d294aa2556d4f21d209b4
zy <zeng_yu_1213@126.com>
2017-08-30 19:54:38 +0800

    create contract model. make migrations and update database.

--- contracts/models.py
+++ contracts/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Contract(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    amount = models.FloatField('contract amount')
+    untreated = models.BooleanField()
+    user_from = models.CharField(max_length=200)
+    user_to = models.CharField(max_length=200)
+    description = models.TextField()
