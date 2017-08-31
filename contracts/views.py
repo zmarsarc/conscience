7ec7de54e99ac896e140659882485b5e0b8d307e
zy <zeng_yu_1213@126.com>
2017-08-31 16:51:52 +0800

    Contract list view. Contract details vies.

--- contracts/views.py
+++ contracts/views.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Contract
 
 # Create your views here.
+
+
+def index(request):
+    contracts = Contract.objects.all()
+    context = {'contracts': contracts}
+    return render(request, 'contracts/index.html', context)
+
+
+def detail(request, contract_id):
+    contract = Contract.objects.get(pk=contract_id)
+    context = {'contract': contract}
+    return render(request, 'contracts/contract_detail.html', context)
